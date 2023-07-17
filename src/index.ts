63882454dd236a9decffe9776047bbd68409f457
Luna <imlvnaa@gmail.com>
2023-07-16 22:52:20 -0400

    refactor to a dynamic system, add memory

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-import { app, Menu, nativeImage, Tray, BrowserWindow, ipcMain } from "electron";
+import {
+  app,
+  Menu,
+  nativeImage,
+  Tray,
+  BrowserWindow,
+  ipcMain,
+  BrowserWindowConstructorOptions,
+} from "electron";
 import fs from "fs";
-import { cpuUsage as _cpuUsage } from "os-utils";
+import { cpuUsage as _cpuUsage, freememPercentage } from "os-utils";
 import { join } from "path";
 
-let window: BrowserWindow;
-let cpuItem: Tray;
-let curText: string = "0%";
+interface textTray extends Tray {
+  text: string;
+}
+interface Entry {
+  window: BrowserWindow;
+  tray: textTray;
+}
+
+const entries: { [key: string]: Entry | undefined } = {
+  cpu: undefined,
+  memory: undefined,
+};
 
 function cpuUsage(): Promise<number> {
   return new Promise((resolve, reject) => {
@@ -14,13 +31,27 @@ function cpuUsage(): Promise<number> {
 }
 
 async function update() {
-  curText = `${await cpuUsage()}%`;
-  window.webContents.send("update", curText);
+  Object.keys(entries).forEach(async (key) => {
+    let percentage: number = 0;
+    switch (key) {
+      case "cpu":
+        percentage = await cpuUsage();
+        break;
+      case "memory":
+        percentage = Math.floor(freememPercentage() * 1000);
+        break;
+    }
+    entries[key]!.tray.text = percentage.toString() + "%";
+    entries[key]!.window.webContents.send(
+      "update",
+      entries[key]!.tray.text,
+      key
+    );
+  });
 }
 
 app.whenReady().then(() => {
-  console.log("ready");
-  window = new BrowserWindow({
+  const windowSettings: BrowserWindowConstructorOptions = {
     width: 60,
     height: 45,
     webPreferences: {
@@ -31,28 +62,35 @@ app.whenReady().then(() => {
     show: false,
     skipTaskbar: true,
     autoHideMenuBar: true,
-  });
+  };
   app.dock.hide();
-  window.removeMenu();
-  window.setMenu(null);
-  window.loadFile(join(__dirname, "index.html"));
-  cpuItem = new Tray(nativeImage.createEmpty());
+
   const contextMenu = Menu.buildFromTemplate([
     { label: "Close", type: "normal" },
   ]);
   contextMenu.items[0].click = () => {
     app.quit();
-    cpuItem.destroy();
-    console.log("quit");
+    Object.values(entries).forEach((entry) => entry!.tray.destroy());
     process.exit(0);
   };
-  cpuItem.setToolTip("This is my application.");
-  cpuItem.setContextMenu(contextMenu);
+
+  Object.keys(entries).forEach((key) => {
+    entries[key] = {
+      window: new BrowserWindow(windowSettings),
+      tray: new Tray(nativeImage.createEmpty()) as textTray,
+    };
+    entries[key]!.tray.text = "0%";
+    entries[key]!.tray.setContextMenu(contextMenu);
+    entries[key]!.tray.setToolTip(`${key} Usage`);
+    entries[key]!.window.removeMenu();
+    entries[key]!.window.setMenu(null);
+    entries[key]!.window.loadFile(join(__dirname, "index.html"));
+  });
 
   setInterval(update, 1000);
   ipcMain.on("done", (event, arg) => {
     let width: number;
-    switch (curText.length) {
+    switch (entries[arg]!.tray.text.length) {
       case 2:
         width = 20;
         break;
@@ -63,11 +101,15 @@ app.whenReady().then(() => {
         width = 40;
         break;
     }
-    window.webContents
-      .capturePage({ height: 30, width: width!, x: 0, y: 0 })
-      .then((image) => {
-        const imageData = image.toDataURL();
-        cpuItem.setImage(nativeImage.createFromDataURL(imageData));
-      });
+    entries[arg]!.window.webContents.capturePage({
+      height: 30,
+      width: width!,
+      x: 0,
+      y: 0,
+    }).then((image) => {
+      const imageData = image.toDataURL();
+      entries[arg]!.tray.setImage(nativeImage.createFromDataURL(imageData));
+      // console.log(`Set entry ${arg} image to ${entries[arg]!.tray.text}`);
+    });
   });
 });
--- src/preload.ts
+++ src/preload.ts
@@ -3,5 +3,5 @@ import { contextBridge, ipcRenderer } from "electron";
 contextBridge.exposeInMainWorld("electronAPI", {
   onUpdate: (callback: (...args: any[]) => void) =>
     ipcRenderer.on("update", callback),
-  done: () => ipcRenderer.send("done"),
+  done: (text: string) => ipcRenderer.send("done", text),
 });
